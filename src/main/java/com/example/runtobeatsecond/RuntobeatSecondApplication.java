94625b71e7dce93341ff3197bb1c662b8ef220d1
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-20 00:04:20 +0900

    login setting

---
+++ src/main/java/com/example/runtobeatsecond/RuntobeatSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.runtobeatsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntobeatSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntobeatSecondApplication.class, args);
+    }
+
+}
